f38912c1be8b82251860d781350385a384b165aa
caiyijun <414065031@qq.com>
2018-05-04 00:41:10 +0800

    引入了viewui,还有在eslintrc中添加规则

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,64 @@
 <template>
-  <div>
-    <nuxt/>
-  </div>
+<div class="layout">
+  <Layout>
+      <Header :style="{position: 'fixed', width: '100%'}">
+          <Menu mode="horizontal" theme="dark" active-name="1">
+              <div class="layout-logo"></div>
+              <div class="layout-nav">
+                  <MenuItem name="1">
+                      <Icon type="ios-navigate"></Icon>
+                      Item 1
+                  </MenuItem>
+              </div>
+          </Menu>
+      </Header>
+      <Content :style="{margin: '88px 20px 0', background: '#fff', minHeight: '500px'}">
+          </nuxt>
+      </Content>
+      <Footer class="layout-footer-center">2011-2016 &copy; TalkingData</Footer>
+  </Layout>
+</div>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data(){
+    return {
+      isCollapsed: false,
+      article:[],
+      categories:[]
+    }
+  },
+ created(){
+   axios.all([
+     axios.get('http://www.likecn.cn/wp-json/wp/v2/posts'),
+     axios.get('http://www.likecn.cn/wp-json/wp/v2/categories'),
+     axios.get('http://www.likecn.cn/wp-json/wp/v2/comments')
+   ])
+   .then(axios.spread((articleres,categoriesres,commentsres)=>{
+    //  console.log(articleres.data);
+     console.log(commentsres)
+     if (articleres.status == 200) {
+       this.article = articleres.data,
+       this.categories = categoriesres.data
+     }
+   }))
+   .catch((error)=>{})
+ },
+ computed: {
+    menuitemClasses: function () {
+      return [
+          'menu-item',
+          this.isCollapsed ? 'collapsed-menu' : ''
+      ]
+    }
+  }
+  
+}
+</script>
+
 
 <style>
 html {
@@ -49,4 +105,31 @@ html {
   color: #fff;
   background-color: #35495e;
 }
+
+
+.layout{
+        border: 1px solid #d7dde4;
+        background: #f5f7f9;
+        position: relative;
+        border-radius: 4px;
+        overflow: hidden;
+    }
+    .layout-logo{
+        width: 100px;
+        height: 30px;
+        background: #5b6270;
+        border-radius: 3px;
+        float: left;
+        position: relative;
+        top: 15px;
+        left: 20px;
+    }
+    .layout-nav{
+        width: 420px;
+        margin: 0 auto;
+        margin-right: 20px;
+    }
+    .layout-footer-center{
+        text-align: center;
+    }
 </style>
